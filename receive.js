e83664a91586d30138f06a4031aaf6d30f741ad9
siv <sivsivsree@gmail.com>
2018-09-19 16:26:06 +0400

    Added Rethink DB using docker

--- receive.js
+++ receive.js
@@ -1,18 +1,35 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib/callback_api'),
+	r = require('rethinkdb'),
+	Promise = require('bluebird')
 
-amqp.connect('amqp://localhost', function (err, conn) {
-    conn.createChannel(function (err, ch) {
-        var q = 'new_task';
 
-        ch.assertQueue(q, { durable: true });
-        ch.prefetch(1);
 
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
 
-        ch.consume(q, function (msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-            ch.ack(msg);
-        }, { noAck: false });
+var connection = null;
+r.connect({ host: '172.17.0.2', port: 28015 }, function (err, conn) {
+	if (err) throw err;
+	connection = conn;
 
-    });
-});
+	amqp.connect('amqp://localhost', function (err, conn) {
+		conn.createChannel(function (err, ch) {
+			var q = 'giveMeOOM';
+
+			ch.assertQueue(q, { durable: true });
+			ch.prefetch(1);
+
+			ch.consume(q, function (msg) {
+				r.table('livedata').insert({uglyData:msg.content.toString()})
+					.run(connection, function (err, result) {
+						if (err) throw err;
+						console.log(JSON.stringify(result, null, 2));
+					})
+				ch.ack(msg);
+			}, { noAck: false });
+
+		});
+	});
+
+
+
+
+})
